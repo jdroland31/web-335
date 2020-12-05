e991bf64b187fbc0c8e825826511978d8bc113a7
Jonathan Roland <jdroland31@yahoo.com>
2020-12-05 18:33:04 -0500

    added code attribution

--- week-8/roland_hello_world.py
+++ week-8/roland_hello_world.py
@@ -1,1 +1,8 @@
+"""
+Title: Assignment 8.2
+Author: Professor Krasso 
+Date: 12/5/2020
+Modified By: Jonathan Roland
+Description: basic Hello World in Python to demonstrate installation success
+"""
 print('Hello World')
