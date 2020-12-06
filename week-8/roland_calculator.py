5efbded719367abddedf1eef10f9fc8290189de2
Jonathan Roland <jdroland31@yahoo.com>
2020-12-05 19:02:05 -0500

    added assignment 8.3

---
+++ week-8/roland_calculator.py
@@ -0,0 +1,18 @@
+#Title: Assignment 8.3
+#Author: Professor Krasso 
+#Date: 12/5/2020
+#Modified By: Jonathan Roland
+#Description: demonstration of Python coding basics with functions, math and outputs
+
+def add(a,b):
+    return a + b
+
+def subtract(a,b):
+    return a - b
+
+def divide(a,b):
+    return a / b
+
+print(add(1,2))
+print(subtract(4,1))
+print(divide(8,2))
--- week-8/roland_hello_world.py
+++ week-8/roland_hello_world.py
@@ -1,8 +1,7 @@
-"""
-Title: Assignment 8.2
-Author: Professor Krasso 
-Date: 12/5/2020
-Modified By: Jonathan Roland
-Description: basic Hello World in Python to demonstrate installation success
-"""
+#Title: Assignment 8.2
+#Author: Professor Krasso 
+#Date: 12/5/2020
+#Modified By: Jonathan Roland
+#Description: basic Hello World in Python to demonstrate installation success
+
 print('Hello World')
